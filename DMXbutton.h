b2cf42cb644553caa96bf6399a5155e56dba872f
Harry Pray IV <harryprayiv@gmail.com>
2017-01-08 03:42:20 -0500

    Update and rename MIDIbutton.h to DMXbutton.h
    Initial renaming from MIDI to DMX.

--- DMXbutton.h
+++ DMXbutton.h
@@ -1,36 +1,36 @@
-#ifndef MIDIbutton_h
-#define MIDIbutton_h
+#ifndef DMXbutton_h
+#define DMXbutton_h
 
 #include "Arduino.h"
 #include "Bounce.h"
 
-extern byte MIDIchannel;
+extern byte DMXchannel;
 
-class MIDIbutton{
-    byte* MC = &MIDIchannel;
+class DMXbutton{
+    byte* MC = &DMXchannel;
 
   public:
     // default constructor
-    MIDIbutton();
+    DMXbutton();
 
     // constructor when only pin & control number are given
-    MIDIbutton(int p, byte num);
+    DMXbutton(int p, byte num);
 
     // " when pin, control number & mode are given
-    MIDIbutton(int p, byte num, byte mod);
+    DMXbutton(int p, byte num, byte mod);
 
     // " pin, control number, on & off values are given
-    MIDIbutton(int p, byte num, byte min, byte max);
+    DMXbutton(int p, byte num, byte min, byte max);
 
     // " pin, control number, on/off values & mode are given
-    MIDIbutton(int p, byte num, byte min, byte max, byte mod);
+    DMXbutton(int p, byte num, byte min, byte max, byte mod);
  
     // destructor
-    ~MIDIbutton();
+    ~DMXbutton();
 
     Bounce* myButt;
     int read(); // returns 1 for fallingEdge, 0 for risingEdge or -1 if neither
-    int send(); // calls read(), sends a MIDI value & returns the control number
+    int send(); // calls read(), sends a DMX value & returns the control number
     byte number;
     byte outLo, outHi;
     byte mode;    
