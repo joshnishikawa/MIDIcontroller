47775118b7d474521d892cc7a0e6633d92fbba44
Harry Pray IV <harryprayiv@gmail.com>
2017-01-08 04:01:43 -0500

    Update MIDIbutton.ino
    MIDI to DMX rename.

--- examples/MIDIbutton/MIDIbutton.ino
+++ examples/MIDIbutton/MIDIbutton.ino
@@ -1,8 +1,8 @@
-#include "MIDIcontroller.h"
-byte MIDIchannel = 5;
+#include "DMXcontroller.h"
+byte DMXchannel = 5;
 const int latchPin = 14; // the pins you have buttons on.
 const int ledPin = 13;   // Set an LED to show the state of the latch button.
-MIDIbutton latchButton(latchPin, 21, 92, 15, 1);
+DMXbutton latchButton(latchPin, 21, 92, 15, 1);
 void setup(){
   pinMode(ledPin, OUTPUT);
 }
