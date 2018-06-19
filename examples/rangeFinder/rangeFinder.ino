43cbac32e9c0c57450f135390aa8c83e827e0b7b
joshnishikawa <joshnishikawa@gmail.com>
2018-06-19 15:25:01 +0900

    Better RangeFinder example
    RangeFinder now retains min / max values and also works for Capacitive Touch values.

--- examples/rangeFinder/rangeFinder.ino
+++ examples/rangeFinder/rangeFinder.ino
@@ -1,16 +1,56 @@
-/*
-  TODO: needs to handle capsens, and auto-store min/max values
-  This is just here for your convenience.
-  You can read any analog sensor and have it
-  printed to the serial monitor while you
-  find the desired upper and lower limits
-  to use in the 'inputRange()' function.
-*/
+#include "Bounce.h"
+int analogPin = 20; // Change to the ANALOG pin you want to use.
+int touchPin = 22;  // Change to the TOUCH pin you want to use.
+Bounce reset = Bounce(12, 50); // You can assign a button to reset the values.
+int newHiA;
+int newLoA;
+int newHiT;
+int newLoT;
 
-void setup(){}
+void setReset(){
+  newHiA = analogRead(analogPin);
+  newLoA = newHiA;
+  newHiT = touchRead(touchPin);
+  newLoT = newHiT;
+  Serial.print("High Analog: "); Serial.println(newHiA);
+  Serial.print("Low Analog : "); Serial.println(newLoA);
+  Serial.print("High Touch: "); Serial.println(newHiT);
+  Serial.print("Low Touch : "); Serial.println(newLoT);
+}
+
+void setup(){
+  pinMode(12, INPUT_PULLUP);
+  setReset();
+}
 
 void loop(){
-  int val = analogRead(23); //Change if needed
-  Serial.println(val);
-  delay(200);
+  reset.update();
+  if(reset.risingEdge()){
+    setReset();
+  }
+  int newVal = analogRead(analogPin);
+  if (newVal > newHiA){
+    newHiA = newVal;
+    Serial.print("High Analog: "); Serial.println(newHiA);
+    Serial.print("Low Analog : "); Serial.println(newLoA);
+  }
+  else if (newVal < newLoA){
+    newLoA = newVal;
+    Serial.print("High Analog: "); Serial.println(newHiA);
+    Serial.print("Low Analog : "); Serial.println(newLoA);
+  }
+
+// UNCOMMENT TO FIND THE RANGE OF A CAPACITIVE TOUCH INPUT
+/*  newVal = touchRead(touchPin);
+  if (newVal > newHiT){
+    newHiT = newVal;
+    Serial.print("High Touch: "); Serial.println(newHiT);
+    Serial.print("Low Touch : "); Serial.println(newLoT);
+  }
+  else if (newVal < newLoT){
+    newLoT = newVal;
+    Serial.print("High Touch: "); Serial.println(newHiT);
+    Serial.print("Low Touch : "); Serial.println(newLoT);
+  }
+*/
 }
